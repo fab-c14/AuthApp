50129136963aefcdb0fefb83e39534a03013da4c
Faisal Bhat <plesim18@gmail.com>
2024-01-02 11:51:39 +0000

    Day 2 Third Commit

--- authapp-api/server.js
+++ authapp-api/server.js
@@ -1,18 +1,78 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt-nodejs')
-const port = 3000 || process.env.PORT
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+// const bcrypt = require('bcrypt-nodejs');
+const port = process.env.PORT || 3000; // Correct order for setting the port
+const app = express();
+const fs = require('fs')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Hash a password
+async function hashPassword(password) {
+    try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        return hashedPassword;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Verify a password
+async function verifyPassword(password, hashedPassword) {
+    try {
+        const match = await bcrypt.compare(password, hashedPassword);
+        return match;
+    } catch (error) {
+        throw error;
+    }
+}
 
 app.get('/', (req, res) => {
     res.send('Hello, Express!');
 });
 
-app.post("/sign",(req,res)=>{
-    res.send("Hey! You Just Tried To SignIn")
+app.post('/sign', (req, res) => {
+    const {email,password} = req.body // Use req.body instead of req.params
+    // console.log(email,password);
+    fs.readFile("users.csv", 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+        } else {
+            // console.log('File content:', data);
+            const lines = data.split('\n');
+            console.log(lines)
+            // lines.forEach(user=>{
+            //     if(email===u)
+            // })
+            
+        }
+    });
+    res.send('Form submitted successfully!');
+});
+
+app.post('/register',async (req,res)=>{
+    const {name,email,password} = req.body 
+    hashedPassword = await hashPassword(password);
+    const formattedData = {name,email,hashedPassword}
+    // console.log(formattedData);
+    // formattedData = [formattedData.name,formattedData.email,formattedData.hashedPassword]
+    // console.log(formattedData)
+    fs.appendFile("users.csv", `${formattedData.name},${formattedData.email},${formattedData.hashedPassword}\n`, (err) => {
+        if (err) {
+            console.error('Error appending to file:', err);
+        } else {
+            console.log('Data appended to file successfully.');
+        }
+    });
+    res.send("Registered User")
 })
-  
-  // Start the server
+
+
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
--- authapp/src/Components/SignIn.js
+++ authapp/src/Components/SignIn.js
@@ -1,60 +1,67 @@
 import { Component } from "react";
 
-class SignIn extends Component{
-    constructor(props){
+class SignIn extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            user:{
-                email:'',
-                password:''
+        this.state = {
+            user: {
+                email: '',
+                password: ''
             }
-        }
+        };
     }
+
     handleEmail = (e) => {
         this.setState({ user: { ...this.state.user, email: e.target.value } });
     };
-    
+
     handlePassword = (e) => {
         this.setState({ user: { ...this.state.user, password: e.target.value } });
     };
-    
-    DetectUser = ()=>{
-        // console.log(this.state.user)
-        fetch("https://3000-fabc14-authapp-cwl7uap5yd6.ws-us107.gitpod.io/sign",{method:'post'})
-        .then(response => response.json())
-        .then(data=>console.log(data))
-        .catch(err=>console.log(err))
+
+    DetectUser = (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+        fetch("https://3000-fabc14-authapp-cwl7uap5yd6.ws-us107.gitpod.io/sign", {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: this.state.user.email,
+                password: this.state.user.password
+            })
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
     }
 
-    render(){
-        const {routeChange} = this.props;
-        return(
+    render() {
+        const { routeChange } = this.props;
+        return (
             <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
                 <main className="pa4 black-80">
-                    <form className="measure center" method="post">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f4 fw6 ph0 mh0">Sign In</legend>
-                        <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" onChange={this.handleEmail}/>
+                    <form className="measure center" onSubmit={this.DetectUser}>
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                            <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+                            <div className="mt3">
+                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" onChange={this.handleEmail} />
+                            </div>
+                            <div className="mv3">
+                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" onChange={this.handlePassword} />
+                            </div>
+                        </fieldset>
+                        <div className="">
+                            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
                         </div>
-                        <div className="mv3">
-                        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" onChange={this.handlePassword}/>
+                        <div className="lh-copy mt3">
+                            <a href="#0" className="f6 link dim black db" onClick={() => routeChange('register')}>Register</a>
                         </div>
-                    </fieldset>
-                    <div className="">
-                        <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" onSubmit={this.DetectUser}/>
-                    </div>
-                    <div className="lh-copy mt3">
-                        <a href="#0" className="f6 link dim black db" onClick={()=>routeChange('register')}>Register</a>
-                    </div>
                     </form>
                 </main>
-        </article>
+            </article>
         )
     }
 }
-
 
 export default SignIn;
